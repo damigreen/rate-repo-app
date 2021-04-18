c1aacc8f6ba953329cfb0f5e30f4fcf235fe3a17
Damilola Faseun <damilola.faseun@gmail.com>
2021-04-18 15:45:18 +0100

    Connected components to application

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Alert } from 'react-native';
+import Main from './src/components/Main';
+
 
 export default function App() {
+  console.log(12334556789073283823672638726873682768372)
   return (
-    <View style={styles.container}>
-      <Text>Hello, world</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Hello, world</Text>
+    //   <StatusBar style="auto" />
+    //   <Pressable
+    //     onPress={() => Alert.alert('You pressed the text!')}
+    //   >
+    //     <Text>You can press me</Text>
+    //   </Pressable>
+    // </View>
+    <Main />
   );
 }
 
