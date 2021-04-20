dfb034060e230d425b9e6f0e714dd966d1c44c1b
Damilola Faseun <damilola.faseun@gmail.com>
2021-04-20 07:28:23 +0100

    using styles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,19 +3,32 @@ import Constants from 'expo-constants';
 import { Text, StyleSheet, View } from 'react-native'
 import RepositoryList from './RepositoryList';
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     marginTop: Constants.statusBarHeight,
     flexGrow: 1,
     flexShrink: 1,
+    backgroundColor: 'purple',
+    color: '#fff'
   },
 });
 
+const BigBlueText = () => {
+  return (
+    <View style={{ padding: 20, marginTop: 20 }}>
+      <Text style={{ color: 'blue', fontSize: 24, fontWeight: '700' }}>
+        Big Blue Text
+      </Text>
+    </View>
+  )
+}
+
 const Main = () => {
   return (
-    <View style={StyleSheet.container}>
+    <View style={styles.container}>
       <Text>Rate Repository Application</Text>
       <RepositoryList />
+      <BigBlueText />
     </View>
   )
 }
