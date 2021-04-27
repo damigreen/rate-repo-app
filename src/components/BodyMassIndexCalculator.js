f11423170918aa970bba39aa7287a7fe7cc10ac1
Damilola Faseun <damilola.faseun@gmail.com>
2021-04-27 22:19:58 +0100

    Added a you validation schema

--- src/components/BodyMassIndexCalculator.js
+++ src/components/BodyMassIndexCalculator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, Pressable, View, StyleSheet } from 'react-native';
 import { Formik, useField } from 'formik';
 import FormikTextInput from './FormikTextInputComponent';
+import * as yup from 'yup';
 
 
 const styles = StyleSheet.create({
@@ -48,6 +49,17 @@ const BodyMassIndexForm = ({ onSubmit }) => {
   );
 };
 
+const validationSchema = yup.object().shape({
+  mass: yup
+    .number()
+    .min(1, 'Weight must be greater or equal to 1')
+    .required('Weight is required'),
+  height: yup
+    .number()
+    .min(0.5, 'Height must be greater or equal to 0.5')
+    .required('Height is required'),
+});
+
 const BodyMassIndexCalculator = () => {
   const onSubmit = values => {
     const mass = parseFloat(values.mass);
@@ -59,7 +71,11 @@ const BodyMassIndexCalculator = () => {
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+    <Formik
+      initialValues={initialValues} 
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
       {( {handleSubmit }) => <BodyMassIndexForm onSubmit={handleSubmit} />}
     </Formik>
   )
